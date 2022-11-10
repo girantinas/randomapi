388b7a4c706d2eb8935412f940332ce5bd5f1ba7
Rohit Agarwal <rohaga@berkeley.edu>
2022-11-09 17:33:15 -0800

    banished executables

--- parser_scripts/enum_parser.py
+++
@@ -1,7 +0,0 @@
-with open('./enum_data', 'r') as f:
-    data = f.read()
-    lines = data.split('\n')[1:]
-    print(lines)
-    final = '\n'.join(["case " + line.split(' ')[4] for line in lines])
-    with open('./enum_data_out', 'w+') as g:
-        g.write(final)
